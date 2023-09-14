b7f0614d43ade037cc7a3ded83950530b9d91d0f
Arthur Mariano <90999445+arthvm@users.noreply.github.com>
2023-09-14 17:28:14 -0300

    Add eraser mode
    This commit adds functionally to the eraser mode button. It also adds
    the basic structure to allow the user to switch between modes.

--- script.js
+++ script.js
@@ -20,6 +20,19 @@ let sliderValue = slider.value;
 let pixelWidth = gridWidth / sliderValue - PIXEL_BORDER * 2;
 let pixelHeight = gridHeight / sliderValue - PIXEL_BORDER * 2;
 
+/***********************************************************SETTINGS VARIABLES***********************************************************/
+
+//****************************COLOR SECTION****************************
+const colorPicker = document.querySelector(".color-picker");
+let colorPickerValue = document.querySelector(".color-picker").value;
+let currentColor = colorPicker.value;
+
+//****************************BUTTONS SECTION****************************
+const settingsButtons = document.querySelectorAll(".mode-btn");
+let activeRainbow = false,
+  activeGrey = false,
+  activeEraser = false;
+
 /***********************************************************GRID SECTION***********************************************************/
 slider.addEventListener("input", () => {
   isSliding = true;
@@ -54,5 +67,92 @@ function UpdateGrid() {
 
     pixel.style.cssText = `width: ${pixelWidth}px; height: ${pixelHeight}px`;
     grid.appendChild(pixel);
+  }
+
+  AddColorListeners();
+}
+
+/***********************************************************COLOR SECTION***********************************************************/
+colorPicker.addEventListener("input", () => {
+  if (!activeEraser) {
+    UpdateColor();
+  }
+});
+
+function UpdateColor() {
+  colorPickerValue = colorPicker.value;
+  currentColor = colorPickerValue;
+}
+
+function AddColorListeners() {
+  const pixels = document.querySelectorAll(".pixel");
+  for (let i = 0; i < pixels.length; i++) {
+    const pixel = pixels[i];
+
+    pixel.addEventListener("click", () => {
+      pixel.style.cssText += `background-color: ${currentColor}`;
+    });
+  }
+}
+
+/***********************************************************COLOR SECTION***********************************************************/
+for (let i = 0; i < settingsButtons.length; i++) {
+  const button = settingsButtons[i];
+
+  button.addEventListener("click", (e) => {
+    const activeButton = document.querySelector(".active");
+
+    if (activeButton == null) {
+      e.target.classList.toggle("active");
+    } else if (activeButton != e.target) {
+      activeButton.classList.toggle("active");
+      e.target.classList.toggle("active");
+    } else {
+      e.target.classList.toggle("active");
+    }
+
+    switch (e.target.id) {
+      case "rainbow-btn":
+        RainbowButton();
+        break;
+      case "grey-btn":
+        GreyButton();
+        break;
+      case "eraser-btn":
+        EraserButton();
+        break;
+    }
+  });
+}
+
+function RainbowButton() {
+  activeRainbow = !activeRainbow;
+  activeEraser = false;
+  activeGrey = false;
+
+  if (activeRainbow) {
+  } else {
+  }
+}
+
+function GreyButton() {
+  activeGrey = !activeGrey;
+  activeRainbow = false;
+  activeEraser = false;
+
+  if (activeGrey) {
+  } else {
+  }
+}
+
+function EraserButton() {
+  activeEraser = !activeEraser;
+  activeRainbow = false;
+  activeGrey = false;
+
+  if (activeEraser) {
+    currentColor = "white";
+  } else {
+    UpdateColor();
   }
 }
